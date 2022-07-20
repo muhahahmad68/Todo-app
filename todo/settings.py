768ab4cac4d84ddd7272a429710c4f1b9f27c400
muhahahmad68 <muhahmadmola09@gmail.com>
2022-07-20 17:41:59 +0100

    tweak setting

--- todo/settings.py
+++ todo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4e@13u!+hf+)whmk*r0hr*qcnvm3w9(ygvmkr6l+qy1=8_j2s^'
+SECRET_KEY = '0f666a90142bf52f5906eb3f39220ffb2d172a3a2342b06d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
